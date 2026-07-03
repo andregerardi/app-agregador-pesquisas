70527dcd5bed42bd1380adda2b0d9f7efe7a2c8f
Dirceu André Gerardi <48441841+andregerardi@users.noreply.github.com>
2026-07-03 13:20:29 -0300

    Update app-agregador-religiao.py

--- app-agregador-religiao.py
+++ app-agregador-religiao.py
@@ -507,13 +507,19 @@ with st.container():
 ########################################################################
  
 with st.container():
-    st.markdown("<div class='turn-band'><span class='lbl'>Explore os dados por turno</span></div>", unsafe_allow_html=True)
+    st.markdown(
+        "<div class='turn-band'><span class='lbl'>Explore os dados por turno</span></div>",
+        unsafe_allow_html=True
+    )
+
     options_turn = st.radio(
         label="",
-        options=['--Selecione o Turno--','Primeiro Turno', 'Segundo Turno'],
+        options=["--Selecione o Turno--", "Primeiro Turno", "Segundo Turno"],
         horizontal=True,
         label_visibility="collapsed",
+        index=0
     )
+
 st.markdown("---")
 
 ############-#########
